cef4aca688d0e5d2f7408a3d283a12a776910c66
nurcann23 <nur_yldz23@outlook.com>
2023-09-19 10:19:18 +0300

    step-6

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firstflut/constants/routes.dart';
+import 'package:firstflut/utilities/show_error_dialog.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
@@ -61,17 +62,32 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
-                // ignore: use_build_context_synchronously
                 Navigator.of(context).pushNamedAndRemoveUntil(
                   notesRoute,
                   (route) => false,
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == "user-not-found") {
-                  devtools.log("User not found !");
+                  await showErrorDialog(
+                    context,
+                    "User not found !",
+                  );
                 } else if (e.code == "wrong-password") {
-                  devtools.log("Wrong password !");
+                  await showErrorDialog(
+                    context,
+                    "Wrong credentials !",
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    "Error: ${e.code}!",
+                  );
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  e.toString(),
+                );
               }
             },
             child: const Text("Login"),
